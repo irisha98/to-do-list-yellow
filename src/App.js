d0ebe07ee54fd9799e7f7ffae7e5dadc2c591bbd
irisha98 <irisha.98>
2025-10-24 18:18:24 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Home from './Home';
-import Mood from './Mood';
-import Inspiration from './Inspiration';
 import Planner from './Planner';
-import Recipes from './Recipes';
-
-import Navbar from './Navbar';
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/Mood' element={<Mood />} />
-        <Route path='/Inspiration' element={<Inspiration />} />
-        <Route path='/Planner' element={<Planner />} />
-        <Route path='/Recipes' element={<Recipes />} />
-      </Routes>
-    </Router>
+  return (<div> 
+        <Planner />
+    </div>
   );
 }
 
